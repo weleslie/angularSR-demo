8684966947ea4682bbe86e487164ecb3d45c8ece
Leslie <2014212810@bupt.edu.cn>
2020-04-09 17:39:54 +0800

    revise the main.py and angularSR.py to be compatible with tf 2.0

--- angularSR.py
+++ angularSR.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+# sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 import os
 import numpy as np
 import h5py
 import time
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.compat.v1.flags.FLAGS
+tf.compat.v1.disable_eager_execution()
 
 
 class AngularSR(object):
@@ -12,7 +14,6 @@ class AngularSR(object):
         self.batch_size = FLAGS.batch_size
         self.learning_rate1 = FLAGS.learning_rate1
         self.learning_rate2 = FLAGS.learning_rate2
-        self.is_train = FLAGS.is_train
         self.epochs = FLAGS.epochs
         self.width = FLAGS.patch_width
         self.height = FLAGS.patch_height
@@ -21,51 +22,51 @@ class AngularSR(object):
         self._init_graph()
 
     def _init_graph(self):
-        self.input = tf.placeholder(tf.float32, shape=(None, self.width, self.height, self.channel), name="image_pairs_input")
-        self.label = tf.placeholder(tf.float32, shape=(None, self.width, self.height, 1), name="groundtruth")
+        self.input = tf.compat.v1.placeholder(tf.float32, shape=(None, self.width, self.height, self.channel),
+                                              name="image_pairs_input")
+        self.label = tf.compat.v1.placeholder(tf.float32, shape=(None, self.width, self.height, 1), name="groundtruth")
 
         self.pred = self.angularSR()
 
-        self.loss = tf.losses.mean_squared_error(self.label, self.pred)
+        self.loss = tf.compat.v1.losses.mean_squared_error(self.label, self.pred)
 
-        self.sess = tf.Session()
+        self.sess = tf.compat.v1.Session()
 
         # load partial parameters
-        self.variable = tf.contrib.framework.get_variables_to_restore(include=["angular_SR"])
-        self.saver_load = tf.train.Saver(self.variable)
-        self.saver_save = tf.train.Saver()
+        self.variable = [var for var in tf.compat.v1.trainable_variables() if "angular_SR" in var.name]
+        self.saver_load = tf.compat.v1.train.Saver(self.variable)
+        self.saver_save = tf.compat.v1.train.Saver(max_to_keep=10)
 
     def angularSR(self):
         with tf.name_scope("angular_SR"):
-            layer1 = tf.keras.layers.Conv2D(64, kernel_size=(9, 9), padding="same", activation='relu',
-                                            kernel_initializer=tf.keras.initializers.random_normal(mean=0.0, stddev=1e-3),
-                                            name="layer1")(self.input)
-            layer2 = tf.keras.layers.Conv2D(32, kernel_size=(5, 5), padding="same", activation="relu",
-                                            kernel_initializer=tf.keras.initializers.random_normal(mean=0.0, stddev=1e-3),
-                                            name="layer2")(layer1)
-            output = tf.keras.layers.Conv2D(1, kernel_size=(5, 5), padding="same",
-                                            kernel_initializer=tf.keras.initializers.random_normal(mean=0.0, stddev=1e-3),
-                                            name="output")(layer2)
+            layer1 = tf.compat.v1.keras.layers.Conv2D(64, kernel_size=(9, 9), padding="same", activation='relu',
+                                                      kernel_initializer=tf.compat.v1.keras.initializers.random_normal(
+                                                          mean=0.0, stddev=1e-3),
+                                                      name="layer1")(self.input)
+            layer2 = tf.compat.v1.keras.layers.Conv2D(32, kernel_size=(5, 5), padding="same", activation="relu",
+                                                      kernel_initializer=tf.compat.v1.keras.initializers.random_normal(
+                                                          mean=0.0, stddev=1e-3),
+                                                      name="layer2")(layer1)
+            output = tf.compat.v1.keras.layers.Conv2D(1, kernel_size=(5, 5), padding="same",
+                                                      kernel_initializer=tf.compat.v1.keras.initializers.random_normal(
+                                                          mean=0.0, stddev=1e-3),
+                                                      name="output")(layer2)
 
         return output
 
     def train(self):
         # setting learning rate hierarchically
-        var1 = tf.trainable_variables()[0:4]
-        var2 = tf.trainable_variables()[4:]
+        var1 = tf.compat.v1.trainable_variables()[0:4]
+        var2 = tf.compat.v1.trainable_variables()[4:]
 
-        opt1 = tf.train.AdamOptimizer(learning_rate=self.learning_rate1)
-        opt2 = tf.train.AdamOptimizer(learning_rate=self.learning_rate2)
+        train_op1 = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learning_rate1).minimize(self.loss,
+                                                                                                 var_list=var1)
+        train_op2 = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learning_rate2).minimize(self.loss,
+                                                                                                 var_list=var2)
 
-        grad = tf.gradients(self.loss, var1 + var2)
-        grad1 = grad[0:len(var1)]
-        grad2 = grad[len(var1):]
+        train_op = tf.compat.v1.group(train_op1, train_op2)
 
-        train_op1 = opt1.apply_gradients(zip(grad1, var1))
-        train_op2 = opt2.apply_gradients(zip(grad2, var2))
-        train_op = tf.group(train_op1, train_op2)
-
-        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.compat.v1.global_variables_initializer())
 
         # load checkpoint
         if self.load():
@@ -122,7 +123,7 @@ class AngularSR(object):
                 temp_error.append(loss)
 
             Loss.append(np.mean(temp_error).squeeze())
-            self.save(i+1)
+            self.save(i + 1)
 
             for k in range(val_batch_number):
                 start = k * self.batch_size
@@ -136,7 +137,7 @@ class AngularSR(object):
                 valid_temp_error.append(loss)
 
             print("Epochs: %d, Loss: %.8f, valid Loss: %.8f, Time: %.4f"
-                  % (i+1, Loss[-1], np.mean(valid_temp_error).squeeze(), time.time() - start_time))
+                  % (i + 1, Loss[-1], np.mean(valid_temp_error).squeeze(), time.time() - start_time))
 
         with h5py.File("loss.h5", 'w') as hf:
             hf.create_dataset("loss", data=Loss)
@@ -177,7 +178,7 @@ class AngularSR(object):
         print("Loss: %.8f" % np.mean(Loss).squeeze())
 
     def load(self):
-        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+        ckpt = tf.compat.v1.train.get_checkpoint_state(FLAGS.checkpoint_dir)
 
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
@@ -188,8 +189,7 @@ class AngularSR(object):
             return False
 
     def save(self, step):
-        if not os.path.exists(FLAGS.checkpoint_dir):
-            os.makedirs(FLAGS.checkpoint_dir)
+        if not os.path.exists(FLAGS.save_dir):
+            os.makedirs(FLAGS.save_dir)
 
-        self.saver_save.save(self.sess, os.path.join(FLAGS.checkpoint_dir, FLAGS.model_name), global_step=step)
-
+        self.saver_save.save(self.sess, os.path.join(FLAGS.save_dir, FLAGS.model_name), global_step=step)
